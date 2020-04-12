8b8073bcf25c7465bd03b8ffbd7c49794014a79b
Xiande Wen <visiande1998@gmail.com>
2020-04-12 23:31:08 +1000

    Setup DictionaryClient

---
+++ src/client/DictionaryClient.java
@@ -0,0 +1,16 @@
+/**
+ * @author Xiande Wen (905003)
+ */
+package client;
+
+public class DictionaryClient {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
