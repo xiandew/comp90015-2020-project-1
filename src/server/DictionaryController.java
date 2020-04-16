887a88524b57b679e5c64f3632663988025cac7d
Xiande Wen <visiande1998@gmail.com>
2020-04-16 12:55:33 +1000

    Add response messages when performing dictionary operations

--- src/server/DictionaryController.java
+++ src/server/DictionaryController.java
@@ -5,13 +5,11 @@ package server;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
 import java.util.HashMap;
 
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import utils.PartOfSpeech;
 
@@ -26,9 +24,8 @@ public class DictionaryController {
 	private void readDictionary(String dictionaryFilePath) throws Exception {
 		this.dictionary = new HashMap<String, HashMap<PartOfSpeech, String>>();
 
-		JSONParser parser = new JSONParser();
 		try {
-			for (Object wordObject : (JSONArray) parser.parse(new FileReader(dictionaryFilePath))) {
+			for (Object wordObject : new JSONArray(new FileReader(dictionaryFilePath))) {
 				JSONObject wordJSONObject = (JSONObject) wordObject;
 				String word = (String) wordJSONObject.get("word");
 				HashMap<PartOfSpeech, String> definitions = new HashMap<>();
@@ -45,27 +42,61 @@ public class DictionaryController {
 			throw new Exception("Please select the dictionary file");
 		} catch (FileNotFoundException e) {
 			throw new Exception(String.format("Error: Dictionary file \"%s\" not found", dictionaryFilePath));
-		} catch (IOException | ParseException e) {
-			throw new Exception("Error: Parse error");
+		} catch (JSONException e) {
+			throw new Exception("Error: Parse error when parsing the dictionary file");
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
 
-	public void addWord(String word, PartOfSpeech pos, String description) {
+	public synchronized String queryWordMeaning(String word) {
+		String response;
+
+		if (this.dictionary.containsKey(word)) {
+			response = "";
+			HashMap<PartOfSpeech, String> meaning = this.dictionary.get(word);
+			for (PartOfSpeech pos : meaning.keySet()) {
+				response += String.format("%s%n%8s%s%n%n", pos, "", meaning.get(pos));
+			}
+		} else {
+			response = String.format("Word \"%s\" not found in the dicitonary", word);
+		}
+
+		return response;
+	}
+
+	public synchronized String addWord(String word, PartOfSpeech pos, String description) {
+		String response;
 		HashMap<PartOfSpeech, String> definitions;
 		if (this.dictionary.containsKey(word)) {
 			definitions = this.dictionary.get(word);
+			if (definitions.containsKey(pos)) {
+				response = String.format("Word \"%s\" of type \"%s\" already exists", word, pos);
+			} else {
+				response = String.format("New definition with type \"%s\" is added to word \"%s\"", pos.toString(),
+						word);
+			}
 		} else {
 			definitions = new HashMap<PartOfSpeech, String>();
+			response = String.format("New word \"%s\" is added to the dictionary", "word");
 		}
+
 		definitions.put(pos, description);
 		this.dictionary.put(word, definitions);
+
+		return response;
 	}
 
-	public void removeWord(String word) throws Exception {
+	public synchronized String removeWord(String word) {
+		String response;
+
 		if (this.dictionary.containsKey(word)) {
 			this.dictionary.remove(word);
+			response = String.format("Word \"%s\" is removed from the dictionary", word);
 		} else {
-			throw new Exception(String.format("Word \"%s\" not found in the dicitonary", word));
+			response = String.format("Word \"%s\" not found in the dicitonary", word);
 		}
+
+		return response;
 	}
 }
