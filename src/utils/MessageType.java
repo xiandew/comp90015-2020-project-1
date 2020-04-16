7691059ea5a4c25cfd47ae90750aad6bd76f0b49
Xiande Wen <visiande1998@gmail.com>
2020-04-16 12:51:07 +1000

    Add MessageType

---
+++ src/utils/MessageType.java
@@ -0,0 +1,10 @@
+package utils;
+
+public enum MessageType {
+	RequestWordMeaning,
+	RequestAddingNewWord,
+	RequestRemovingWord,
+	ResponseWordMeaning,
+	ResponseAddingNewWord,
+	ResponseRemovingWord
+}
